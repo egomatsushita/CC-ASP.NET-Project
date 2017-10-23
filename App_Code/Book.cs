d67458856c7273d6e92c360c7f4da68e85a55114
egomatsushita <eduardo.go.rj@gmail.com>
2017-10-22 20:34:15 -0400

    Initial setup of app_code folder

---
+++ App_Code/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Book
+/// </summary>
+public class Book
+{
+    public Book()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
---
+++ App_Code/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BookRepository
+/// </summary>
+public class BookRepository
+{
+    public BookRepository()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
