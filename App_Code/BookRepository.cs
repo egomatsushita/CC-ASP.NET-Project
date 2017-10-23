a928e195a0804cfa9d905442478216240c6dae1c
egomatsushita <eduardo.go.rj@gmail.com>
2017-10-22 20:53:56 -0400

    Setup BookRepository class

--- App_Code/BookRepository.cs
+++ App_Code/BookRepository.cs
@@ -8,10 +8,25 @@ using System.Web;
 /// </summary>
 public class BookRepository
 {
+    List<Book> bookList;
+
     public BookRepository()
     {
-        //
-        // TODO: Add constructor logic here
-        //
+        bookList = new List<Book>();
+    }
+
+    public List<Book> GetAllBooks()
+    {
+        return bookList;
+    }
+
+    public void Add(Book aBook)
+    {
+        bookList.Add(aBook);
+    }
+
+    public void Delete(Book aBook)
+    {
+        bookList.Remove(aBook);
     }
 }
