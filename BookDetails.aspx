c78ead348ba4bd4f7d85c3bdc8d086bd9d08904e
egomatsushita <eduardo.go.rj@gmail.com>
2017-12-17 13:41:27 -0500

    Create book details page

---
+++ BookDetails.aspx
@@ -0,0 +1,16 @@
+﻿<%--*******************************
+    * Name: Eduardo Go Matsushita *
+    * Student Id: 300904663       *
+    *******************************--%>
+
+<%@ Page Title="Book Details" Language="C#" MasterPageFile="~/LibraryCollection.master" AutoEventWireup="true" CodeFile="BookDetails.aspx.cs" Inherits="BookDetails" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolderContent" Runat="Server">
+</asp:Content>
+
+<%--*******************************
+    * Name: Eduardo Go Matsushita *
+    * Student Id: 300904663       *
+    *******************************--%>
---
+++ BookDetails.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//*******************************
+//* Name: Eduardo Go Matsushita *
+//* Student Id: 300904663       *
+//*******************************
+
+public partial class BookDetails : System.Web.UI.Page
+{
+    protected void Page_PreInit(object sender, EventArgs e)
+    {
+        if (Request.Cookies["Theme"] != null)
+        {
+            this.Theme = Session["Theme"] as String;
+        }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
+
+//*******************************
+//* Name: Eduardo Go Matsushita *
+//* Student Id: 300904663       *
+//*******************************
